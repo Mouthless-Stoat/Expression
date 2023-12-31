f25960df6d5a8eb698dab47aaa2d26dd657cdbc8
khanhfg <nphuyanh@gmail.com>
2023-12-31 18:17:43 -0400

    Remove build

--- src-but-js/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.error = void 0;
-function error(...message) {
-    console.log(...message);
-    throw new Error("Error");
-}
-exports.error = error;
