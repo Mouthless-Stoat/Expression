2b3c72ab7d7a481715661dced0659e8d70f67fc8
khanhfg <nphuyanh@gmail.com>
2024-01-04 18:14:19 -0400

    Change Unary Op code to use enviroment clone method

--- src/runtime/UnaryOp.ts
+++ src/runtime/UnaryOp.ts
@@ -1,7 +1,7 @@
 import Enviroment from "./enviroment"
 import { TokenType } from "../frontend/lexer"
 import { MKBOOL, NULLVAL, NumberVal, RuntimeVal, ValueType, isValueTypes, valueName } from "./value"
-import { Expr, Identifier, NodeType, isNodeType } from "../frontend/ast"
+import { Expr, Identifier, IndexExpr, NodeType, isNodeType } from "../frontend/ast"
 import { evaluate } from "./interpreter"
 import { error } from "../utils"
 
@@ -48,6 +48,23 @@ export const PreUnaryOp: Record<PreUnaryOpType, (expr: Expr, env: Enviroment) =>
     },
     "*": (expr, env) => {
         if (isNodeType(expr, NodeType.Identifier)) return env.unsignVar((expr as Identifier).symbol)
+        if (isNodeType(expr, NodeType.IndexExpr)) {
+            const cloneEnv = env.clone() // clone the env to redo some cal
+
+            const indexExpr = expr as IndexExpr // cast the expr
+            const oldVal = evaluate(indexExpr, env) // get the old value
+
+            // use a clone env to not do the eval using the same env
+            // we can safely use these value without checking because the previous retrival
+            // pass if we get here
+            const indexable = evaluate(indexExpr.expr, cloneEnv).value
+            const index = evaluate(indexExpr.index, cloneEnv).value
+
+            // remove the element from the list
+            indexable.splice(index, 1)
+
+            return oldVal
+        }
         return error("TypeError: Cannot unsign")
     },
 }
