c6242a4e4c290ee78befb5ff497ea7516518d4a7
Mouthless Stoat <nphuyanh@gmail.com>
2024-01-17 15:23:22 -0400

    Actually clone list when repeating

--- src/runtime/value.ts
+++ src/runtime/value.ts
@@ -463,7 +463,10 @@ export class ListVal implements RuntimeVal {
     mul(rhs: RuntimeVal): RuntimeVal | undefined {
         if (isValueTypes(rhs, ValueType.Number)) {
             const num = Math.round((rhs as NumberVal).value)
-            const list = new Array(Math.abs(num)).fill(this.value).flat()
+            const list = new Array(Math.abs(num))
+                .fill(1)
+                .map((_) => deepClone(this.value))
+                .flat()
             if (num < 0) list.reverse()
             return new ListVal(list)
         } else if (isValueTypes(rhs, ValueType.List)) {
